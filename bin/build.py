ba25954d45d08cf3ad4e82fc7c01a3eaf27bcb1f
Dan Tovbein <dtovbein@gmail.com>
2023-02-17 08:42:35 -0300

    Change to CSS variables
    - Also sort token names
    - Divide primitive and component specific tokens in sections

--- bin/build.py
+++ bin/build.py
@@ -5,7 +5,7 @@ import os
 import re
 
 JSON_TOKENS_FILE = 'tokens.json'
-SASS_TOKENS_FILE = '_tokens.scss'
+TOKENS_FILE = '_tokens.css'
 
 def parse_token_value(value):
     if('{' in value):
@@ -22,20 +22,33 @@ if __name__ == '__main__':
             json_data = json.load(json_file)
 
             if 'tokenSetOrder' in json_data['$metadata']:
-                print('Open and update {0} file'.format(SASS_TOKENS_FILE))
-                tokens_scss = open('{0}/src/{1}'.format(os.getcwd(), SASS_TOKENS_FILE), 'w')
+                print('Open and update {0} file'.format(TOKENS_FILE))
+                tokens_scss = open('{0}/src/{1}'.format(os.getcwd(), TOKENS_FILE), 'w')
+
+                tokens_component_specific = []
+
+                tokens_scss.write(':root {\n')
 
                 for tokens in json_data['$metadata']['tokenSetOrder']:
                     print('Parse tokens from {0}'.format(JSON_TOKENS_FILE))
-                    for token in json_data[tokens]:
+                    tokens_scss.write('\t/* Primitives */\n')
+                    for token in sorted(json_data[tokens]):
                         token_data = json_data[tokens][token]
 
-                        if 'value' in token_data:
-                            tokens_scss.write('${0}: {1};\n'.format(token, parse_token_value(token_data['value'])))
+                        value = parse_token_value(token_data['value'])
+                        line = '\t--{0}: {1};\n'.format(token, parse_token_value(token_data['value']))
+                        if "$" not in value:
+                            tokens_scss.write(line)
+                        else:
+                            tokens_component_specific.append(line)
 
+                    tokens_scss.write('\t/* Component Specific */\n')
+                    for token in tokens_component_specific:
+                        tokens_scss.write(token)
+
+                tokens_scss.write('}')
                 tokens_scss.close()
-                print('Save a new version of {0} file'.format(SASS_TOKENS_FILE))
-
+                print('Save a new version of {0} file'.format(TOKENS_FILE))
     else:
         print('The {0} file does not exist'.format(JSON_TOKENS_FILE))
         exit(1)
