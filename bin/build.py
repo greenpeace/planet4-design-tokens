c4e2f37a7c51198ac12657b18bbcb383a8f6e9f2
Dan Tovbein <dtovbein@gmail.com>
2023-05-12 10:25:15 -0300

    Implement typography tokens
    - Using the primitive ones for now

--- bin/build.py
+++ bin/build.py
@@ -7,16 +7,41 @@ import re
 JSON_TOKENS_FILE = 'tokens.json'
 TOKENS_FILE = '_tokens.scss'
 
+def parse_token_name(value):
+    # Remove empty spaces, replace to `-` and convert to lower case.
+    return re.sub('[ ]+', '-', value).lower()
+
 def parse_token_value(value, css_variable):
-    if('{' in value):
-        if(True == css_variable):
-            return 'var(--{0})'.format(re.sub('[{}]+', '', value))
-        else:
-            return '${0}'.format(re.sub('[{}]+', '', value))
+    if type(value) is dict:
+        return None
+    else:
+        if('{' in value):
+            if(True == css_variable):
+                return 'var(--{0})'.format(re.sub('[{}]+', '', value))
+            else:
+                return '${0}'.format(re.sub('[{}]+', '', value))
 
-    return value.lower()
+    # This means an hexa color value
+    if('#' in value):
+        return value.lower()
+    else:
+        return '"{0}"'.format(value)
 
-def write_lines(token_set_order, tokens_stylesheet, css_variable = True):
+def parse_line(token_name, value, tab, css_variable):
+    token_name = parse_token_name(token_name)
+    if True == css_variable:
+        return '{0}--{1}: {2};\n'.format(tab, token_name, value)
+    else:
+        return '{0}${1}: {2};\n'.format(tab, token_name, value)
+
+def write_line(value, line, tokens_primitives, tokens_component_specific):
+    # Having `var()` or `$` means that are component spcific token
+    if "var" not in value and "$" not in value:
+        tokens_primitives.write(line)
+    else:
+        tokens_component_specific.append(line)
+
+def read_json_data(token_set_order, tokens_stylesheet, css_variable = True):
     tokens_component_specific = []
     tab = ''
 
@@ -25,27 +50,39 @@ def write_lines(token_set_order, tokens_stylesheet, css_variable = True):
         tab = '  '
 
     for tokens in token_set_order:
-        print('Parse tokens from {0}'.format(JSON_TOKENS_FILE))
+        print('Parse tokens from {0}\n'.format(JSON_TOKENS_FILE))
 
+        # Primitive Tokens
         tokens_stylesheet.write('{0}/* Primitives */\n'.format(tab))
         for token_name in sorted(json_data[tokens]):
             token_data = json_data[tokens][token_name]
 
-            value = parse_token_value(token_data['value'], css_variable)
+            # Group doesn't include the value as field
+            if 'value' in token_data:
+                value = parse_token_value(token_data['value'], css_variable)
 
-            if True == css_variable:
-                line = '{0}--{1}: {2};\n'.format(tab, token_name, value)
+                if value is None:
+                    print('The value of {0} cannot be parsed as a regular token'.format(token_name))
+                else:
+                    line = parse_line(token_name, value, tab, css_variable)
+                    write_line(value, line, tokens_stylesheet, tokens_component_specific)
             else:
-                line = '${0}: {1};\n'.format(token_name, value)
+                print('Parse tokens related to "{0}"'.format(token_name))
 
-            if "var" not in value and "$" not in value:
-                tokens_stylesheet.write(line)
-            else:
-                tokens_component_specific.append(line)
+                # Find the associated value of the token_name
+                for _ in json_data[tokens]:
+                    if 'value' in json_data[tokens][_] and token_name == json_data[tokens][_]['value']:
+                        token_name = parse_token_name(_)
 
+                for token_property in token_data:
+                    value = token_data[token_property]['value']
+                    line = parse_line('{0}--{1}'.format(token_property, token_name), value, tab, css_variable)
+                    write_line(value, line, tokens_stylesheet, tokens_component_specific)
+
+        # Component Specific Tokens
         tokens_stylesheet.write('{0}/* Component Specific */\n'.format(tab))
-        for token_name in tokens_component_specific:
-            tokens_stylesheet.write(token_name)
+        for token in tokens_component_specific:
+            tokens_stylesheet.write(token)
 
     if True == css_variable:
         tokens_stylesheet.write('}\n')
@@ -65,9 +102,9 @@ if __name__ == '__main__':
 
                 tokens_stylesheet.write('/* This is an auto-generated file. */\n\n')
                 tokens_stylesheet.write('/* CSS Variables */\n')
-                write_lines(json_data['$metadata']['tokenSetOrder'], tokens_stylesheet)
+                read_json_data(json_data['$metadata']['tokenSetOrder'], tokens_stylesheet)
                 tokens_stylesheet.write('\n/* SASS Variables */\n')
-                write_lines(json_data['$metadata']['tokenSetOrder'], tokens_stylesheet, False)
+                read_json_data(json_data['$metadata']['tokenSetOrder'], tokens_stylesheet, False)
 
                 tokens_stylesheet.close()
                 print('Save a new version of {0} file'.format(TOKENS_FILE))
